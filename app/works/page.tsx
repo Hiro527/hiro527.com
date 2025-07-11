15a7de54b66758aa0195e9b263c2bba8f7a7a8db
Hiro527 <hiro527.dev@gmail.com>
2025-07-11 16:54:27 +0900

    Fix: 新しいWorksページコンポーネントを作成

---
+++ app/works/page.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from '@mui/material';
+
+export default function Home() {
+    return (
+        <Box>
+            <Typography>Works</Typography>
+        </Box>
+    );
+}
